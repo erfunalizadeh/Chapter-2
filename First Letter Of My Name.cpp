7665b2574931f9e4af7ea3f7bd909b71fd94c404
erfunalizadeh <117520554+erfunalizadeh@users.noreply.github.com>
2022-11-06 00:55:35 +0330

    Add files via upload

---
+++ First Letter Of My Name.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void Drawletter();
+int main(){
+	Drawletter();
+return 0;
+}
+void Drawletter(){
+	//My Name Is Erfan
+	cout<<"\n ***** ";
+	cout<<"\n * ";
+	cout<<"\n ***** ";
+	cout<<"\n * ";
+	cout<<"\n ***** ";
+}
