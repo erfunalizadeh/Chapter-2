941f0201f855dff73d7ff8600a5b4c8a4006cc51
erfunalizadeh <117520554+erfunalizadeh@users.noreply.github.com>
2022-11-05 16:26:50 +0330

    Add files via upload

---
+++ Convert Farenheit To Celcius.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float Farenheit;
+cout<<"\n Enter The Degree Of Temperature In Farenheit :";
+cin>>Farenheit;
+cout<<"\n Degree Of Temperature In Celcius :"<<(Farenheit-32)/1.8;
+return 0;
+}
