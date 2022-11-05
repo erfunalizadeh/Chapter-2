8b87ea2db7184b18167de0970f8d4abc3f3b33c0
erfunalizadeh <117520554+erfunalizadeh@users.noreply.github.com>
2022-11-06 01:02:12 +0330

    Add files via upload

---
+++ Age To Months And Days.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int Age;
+	cout<<"\n Please Enter Your Age :";
+	cin>>Age;
+	cout<<"\n Months :"<<Age*12;
+	cout<<"\n Days :"<<Age*365;
+	return 0;
+}
