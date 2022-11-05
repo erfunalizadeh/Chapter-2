35e512efcc865512b7c2d289eebfa790156f6dd5
erfunalizadeh <117520554+erfunalizadeh@users.noreply.github.com>
2022-11-06 00:56:51 +0330

    Add files via upload

---
+++ 2 Numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,num2;
+	cout<<"\n Enter num1 & num2+
+	 :";
+	cin>>num1>>num2;
+	cout<<"\n Sum is :"<<num1+num2;
+	cout<<"\n Difference is :"<<num1-num2;
+	cout<<"\n Produst is :"<<num1*num2;
+	cout<<"\n Qoutient is :"<<num1%num2;
+return 0;
+}
